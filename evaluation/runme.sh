bc144482f2f1aa25736c9a85ea6b825ba764b594
ygorg <ygor.gallina@univ-nantes.fr>
2024-03-01 15:40:57 +0100

    Fixed Jean Zay support in evaluation

--- evaluation/runme.sh
+++ evaluation/runme.sh
@@ -3,7 +3,7 @@ git clone https://github.com/coastalcph/lex-glue
 cd lex-glue
 git checkout 20ad76a6d9da7794daddd94f23701c486ebad29f
 echo "-- Applying patch --"
-git apply --verbose ../0001-Update-experiment-file-to-work-without-cache_dir.patch
+git apply --verbose ../0001-JZ-Support-and-custom-cache.patch
 
 echo "-- Loading data --"
 python3 ../download_data.py
